3db4c4dcd2dc96aeddefa67d0f64eeefa29587c1
SohPandaa <sohpandaa@gmail.com>
2025-01-14 13:35:19 -0500

    remove config

--- src/main/java/net/sohpandaa/geometracoins/GeometraCoins.java
+++ src/main/java/net/sohpandaa/geometracoins/GeometraCoins.java
@@ -48,7 +48,7 @@ public class GeometraCoins {
         modEventBus.addListener(this::addCreative);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
